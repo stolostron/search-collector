cded0c920b3c0bcf87a16e7bcc36e0d097bfd119
Ethan P. Swartzentruber <Ethan.Swartzentruber@ibm.com>
2019-03-15 15:55:52 -0400

    Switch to all dynamic informers (#35)

--- main.go
+++ main.go
@@ -4,17 +4,14 @@ import (
 	"flag"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/golang/glog"
 	"github.ibm.com/IBMPrivateCloud/search-collector/pkg/transforms"
-	machineryV1 "k8s.io/apimachinery/pkg/apis/meta/v1" // This one has the interface
+	machineryV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/dynamic/dynamicinformer"
-	"k8s.io/client-go/informers"
-	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -35,13 +32,11 @@ func main() {
 	glog.Info("Starting Data Collector")
 
 	// Create in/out channels for the transformer
-	transformInput := make(chan machineryV1.Object)
-	transformDynamicInput := make(chan *unstructured.Unstructured)
+	transformInput := make(chan *unstructured.Unstructured)
 	transformOutput := make(chan transforms.Node)
 	t := transforms.Transformer{
-		Input:        transformInput,
-		DynamicInput: transformDynamicInput,
-		Output:       transformOutput,
+		Input:  transformInput,
+		Output: transformOutput,
 	}
 
 	// Start the transformer with 1 threads doing transformation work.
@@ -63,12 +58,6 @@ func main() {
 		glog.Fatal("Error Constructing Client From Config: ", err)
 	}
 
-	// Initialize the normal client, used for CRUD operations on default k8s resources
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		glog.Fatal("Cannot Construct Kubernetes Client From Config: ", err)
-	}
-
 	// Initialize the dynamic client, used for CRUD operations on nondeafult k8s resources
 	dynamicClientset, err := dynamic.NewForConfig(config)
 	if err != nil {
@@ -76,7 +65,6 @@ func main() {
 	}
 
 	// Create informer factories
-	factory := informers.NewSharedInformerFactory(clientset, 0)                            // factory for building informer objects used with default k8s resources
 	dynamicFactory := dynamicinformer.NewDynamicSharedInformerFactory(dynamicClientset, 0) // factory for building dynamic informer objects used with CRDs and arbitrary k8s objects
 
 	// Create special type of client used for discovering resource types
@@ -112,6 +100,7 @@ func main() {
 
 	// Use handy converter function to convert into GroupVersionResource objects, which we need in order to make informers
 	gvrList, err := discovery.GroupVersionResources(supportedResources)
+
 	if err != nil {
 		glog.Fatal("Could not read api-resource object", err) // TODO pretty sure this would be fatal but I don't actually know how to produce it, so... we'll see! :)
 	}
@@ -119,44 +108,21 @@ func main() {
 	stopper := make(chan struct{}) // We just have one stopper channel that we pass to all the informers, we always stop them together.
 	defer close(stopper)
 
+	// Now that we have a list of all the GVRs for resources we support, make dynamic informers for each one, set them up to pass their data into the transformer, and start them.
 	for gvr := range gvrList {
-		genericInformer, err := factory.ForResource(gvr) // Attempt to create standard informer. This will return an erro on non-default k8s resources.
-		var informer cache.SharedIndexInformer
-		if err != nil {
-			if isNoInformerError(err) { // This will be true when the resource is nondefault
-				// In this case we need to create a dynamic informer, since there is no built in informer for this type.
-				dynamicInformer := dynamicFactory.ForResource(gvr)
-				glog.Infof("Created informer for %s \n", gvr.String())
-				// Set up handler to pass this informer's resources into transformer
-				informer = dynamicInformer.Informer()
-				informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-					AddFunc: func(obj interface{}) {
-						resource := obj.(*unstructured.Unstructured)
-						t.DynamicInput <- resource // Send pod into the transformerChannel
-					},
-				})
+		// In this case we need to create a dynamic informer, since there is no built in informer for this type.
+		dynamicInformer := dynamicFactory.ForResource(gvr)
+		glog.Infof("Created informer for %s \n", gvr.String())
+		// Set up handler to pass this informer's resources into transformer
+		informer := dynamicInformer.Informer()
+		informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				resource := obj.(*unstructured.Unstructured)
+				t.Input <- resource // Send resource into the transformer input channel
+			},
+		})
 
-				go informer.Run(stopper)
-			} else { // Some other error
-				glog.Errorf("Unable to create informer for %s - %v\n", gvr.String(), err)
-			}
-		} else {
-			// TODO success case, run the informer
-			// Set up handler to pass this informer's resources into transformer
-			informer = genericInformer.Informer()
-
-			informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-				AddFunc: func(obj interface{}) {
-					resource := obj.(machineryV1.Object)
-					transformInput <- resource // Send pod into the transformerChannel
-				},
-			})
-			informer := genericInformer.Informer()
-			glog.Infof("Created informer for %s \n", gvr.String())
-
-			go informer.Run(stopper)
-		}
-
+		go informer.Run(stopper)
 	}
 
 	receiver(transformOutput) // Would be the sender. For now, just a simple function to print it for testing.
@@ -178,9 +144,4 @@ func receiver(transformOutput chan transforms.Node) {
 		}
 		glog.Info("Received: " + kind + " " + name)
 	}
-}
-
-// Checks whether the given error is a "no informer found" error
-func isNoInformerError(e error) bool {
-	return strings.HasPrefix(e.Error(), "no informer found for")
 }
--- pkg/transforms/transformer.go
+++ pkg/transforms/transformer.go
@@ -1,14 +1,14 @@
 package transforms
 
 import (
+	"encoding/json"
 	"errors"
 
 	"github.com/golang/glog"
 	apps "k8s.io/api/apps/v1"
 	batch "k8s.io/api/batch/v1"
 	batchBeta "k8s.io/api/batch/v1beta1"
-	core "k8s.io/api/core/v1"                          // This one has all the concrete types
-	machineryV1 "k8s.io/apimachinery/pkg/apis/meta/v1" // This one has the interface
+	core "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -30,9 +30,8 @@ type Node struct {
 // Object that handles transformation of k8s objects.
 // To use, create one, call Start(), and begin passing in objects.
 type Transformer struct {
-	Input        chan machineryV1.Object         // Put default k8s objects into here.
-	DynamicInput chan *unstructured.Unstructured // Put nondefault k8s objects into here.
-	Output       chan Node                       // And recieve your redisgraph nodes from here.
+	Input  chan *unstructured.Unstructured // Put your k8s resources in here.
+	Output chan Node                       // And recieve your redisgraph nodes from here.
 	// TODO add stopper channel?
 }
 
@@ -45,54 +44,132 @@ func (t Transformer) Start(numRoutines int) error {
 
 	// start numRoutines threads to handle transformation.
 	for i := 0; i < numRoutines; i++ {
-		go transformRoutine(t.Input, t.DynamicInput, t.Output)
+		go transformRoutine(t.Input, t.Output)
 	}
 	return nil
 }
 
 // This function is to be run as a goroutine that processes k8s objects into Nodes, then spits them out into the output channel.
-func transformRoutine(input chan machineryV1.Object, dynamicInput chan *unstructured.Unstructured, output chan Node) {
-	defer handleRoutineExit(input, dynamicInput, output)
+// If anything goes wrong in here that requires you to skip the current resource, call panic() and the routine will be spun back up by handleRoutineExit and the bad resource won't be in there because it was already taken out by the previous run.
+func transformRoutine(input chan *unstructured.Unstructured, output chan Node) {
+	defer handleRoutineExit(input, output)
 	glog.Info("Starting transformer routine")
 	// TODO not exactly sure, but we may need a stopper channel here.
 	for {
 		var transformed Node
 
-		// Read from one of the two input channels
-		select {
-		case resource := <-input: // Reading a default k8s object from the normal channel
-			// Type switch over input and call the appropriate transform function
-			switch typedResource := resource.(type) {
-			case *core.ConfigMap:
-				transformed = transformConfigMap(typedResource)
-			case *batchBeta.CronJob:
-				transformed = transformCronJob(typedResource)
-			case *apps.DaemonSet:
-				transformed = transformDaemonSet(typedResource)
-			case *apps.Deployment:
-				transformed = transformDeployment(typedResource)
-			case *batch.Job:
-				transformed = transformJob(typedResource)
-			case *core.Namespace:
-				transformed = transformNamespace(typedResource)
-			case *core.Node:
-				transformed = transformNode(typedResource)
-			case *core.PersistentVolume:
-				transformed = transformPersistentVolume(typedResource)
-			case *core.Pod:
-				transformed = transformPod(typedResource)
-			case *apps.ReplicaSet:
-				transformed = transformReplicaSet(typedResource)
-			case *core.Secret:
-				transformed = transformSecret(typedResource)
-			case *core.Service:
-				transformed = transformService(typedResource)
-			case *apps.StatefulSet:
-				transformed = transformStatefulSet(typedResource)
-			default:
-				transformed = transformCommon(typedResource)
+		resource := <-input // Read from the input channel
+		// Pull out the kind and use marshal/unmarshal to convert to the right type.
+		j, err := json.Marshal(resource)
+		if err != nil {
+			panic(err) // Will be caught by handleRoutineExit
+		}
+		switch kind := resource.GetKind(); kind {
+
+		case "ConfigMap":
+			typedResource := core.ConfigMap{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
 			}
-		case resource := <-dynamicInput: // Reading a nondefault object from the dynamic channel
+			transformed = transformConfigMap(&typedResource)
+
+		case "CronJob":
+			typedResource := batchBeta.CronJob{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformCronJob(&typedResource)
+
+		case "DaemonSet":
+			typedResource := apps.DaemonSet{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformDaemonSet(&typedResource)
+
+		case "Deployment":
+			typedResource := apps.Deployment{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformDeployment(&typedResource)
+
+		case "Job":
+			typedResource := batch.Job{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformJob(&typedResource)
+
+		case "Namespace":
+			typedResource := core.Namespace{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformNamespace(&typedResource)
+
+		case "Node":
+			typedResource := core.Node{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformNode(&typedResource)
+
+		case "PersistentVolume":
+			typedResource := core.PersistentVolume{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformPersistentVolume(&typedResource)
+
+		case "Pod":
+			typedResource := core.Pod{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformPod(&typedResource)
+
+		case "ReplicaSet":
+			typedResource := apps.ReplicaSet{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformReplicaSet(&typedResource)
+
+		case "Secret":
+			typedResource := core.Secret{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformSecret(&typedResource)
+
+		case "Service":
+			typedResource := core.Service{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformService(&typedResource)
+
+		case "StatefulSet":
+			typedResource := apps.StatefulSet{}
+			err = json.Unmarshal(j, &typedResource)
+			if err != nil {
+				panic(err) // Will be caught by handleRoutineExit
+			}
+			transformed = transformStatefulSet(&typedResource)
+		default:
 			transformed = transformUnstructured(resource)
 		}
 
@@ -104,12 +181,12 @@ func transformRoutine(input chan machineryV1.Object, dynamicInput chan *unstruct
 // Handles a panic from inside transformRoutine.
 // If the panic was due to an error, starts another transformRoutine with the same channels as this one.
 // If not, just lets it die.
-func handleRoutineExit(input chan machineryV1.Object, dynamicInput chan *unstructured.Unstructured, output chan Node) {
+func handleRoutineExit(input chan *unstructured.Unstructured, output chan Node) {
 	// Recover and check the value. If we are here because of a panic, something will be in it.
 	if r := recover(); r != nil { // Case where we got here from a panic
 		glog.Errorf("Error in transformer routine: %v\n", r)
 
 		// Start up a new routine with the same channels as the old one. The bad input will be gone since the old routine (the one that just crashed) took it out of the channel.
-		go transformRoutine(input, dynamicInput, output)
+		go transformRoutine(input, output)
 	}
 }
