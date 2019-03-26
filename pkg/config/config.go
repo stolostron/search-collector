708e98c1ee636fe3b98cefdb6780eabc0237cc75
JORGE L. PADILLA <jpadilla@us.ibm.com>
2019-03-26 11:58:34 -0400

    Set config from environment variables (#47)
    * Set config from environment variables
    
    * Add warning if config.json is missing.
    
    * env vars
    
    * Fix env var overrides

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,9 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/golang/glog"
 	"github.com/tkanos/gonfig"
+)
+
+const (
+	DEFAULT_CLUSTER_NAME   = "localtest"
+	DEFAULT_AGGREGATOR_URL = "http://localhost:3010"
 )
 
 // Define a config type for gonfig to hold our config properties.
@@ -29,10 +35,31 @@ func init() {
 	}
 	defer glog.Flush() // This should ensure that everything makes it out on to the console if the program crashes.
 
-	err = gonfig.GetConf(*FilePath, &Cfg)
-	if err != nil { // Check for stinkies, if this fails we can't really recover
-		fmt.Println("Error reading config file:", err) // Uses fmt.Println in case something is wrong with glog args
-		os.Exit(1)
+	// Load default config from ./config.json.
+	// These can be overriden in the next step if environment variables are set.
+	if _, err := os.Stat(filepath.Join(".", "config.json")); !os.IsNotExist(err) {
+		err = gonfig.GetConf(*FilePath, &Cfg)
+		if err != nil {
+			fmt.Println("Error reading config file:", err) // Uses fmt.Println in case something is wrong with glog args
+		}
+		glog.Info("Successfully read from config file: ", *FilePath)
+	} else {
+		glog.Warning("Missing config file: ./config.json.")
 	}
-	glog.Info("Successfully read from config file: ", *FilePath)
+
+	// If environment variables are set, use those values instead of ./config.json
+	if clusterName := os.Getenv("CLUSTER_NAME"); clusterName != "" {
+		glog.Info("Using CLUSTER_NAME from environment: ", clusterName)
+		Cfg.ClusterName = clusterName
+	} else if Cfg.ClusterName == "" {
+		glog.Warning("No ClusterName from file or environment, using default ClusterName: ", DEFAULT_CLUSTER_NAME)
+		Cfg.ClusterName = DEFAULT_CLUSTER_NAME
+	}
+	if aggregatorURL := os.Getenv("AGGREGATOR_URL"); aggregatorURL != "" {
+		glog.Info("Using AGGREGATOR_URL from environment: ", aggregatorURL)
+		Cfg.AggregatorURL = aggregatorURL
+	} else if Cfg.AggregatorURL == "" {
+		glog.Warning("No AggregatorURL from file or environment, using default AggregatorURL: ", DEFAULT_AGGREGATOR_URL)
+		Cfg.AggregatorURL = DEFAULT_AGGREGATOR_URL
+	}
 }
