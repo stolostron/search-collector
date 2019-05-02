991374a1387776e7fe680bb75e33c3c0e28e8de2
Mitchell Durham <mdurham@us.ibm.com>
2019-05-02 16:13:25 -0400

    loop through pod continaer status to match pod status from kubectl (#88)

--- pkg/transforms/pod.go
+++ pkg/transforms/pod.go
@@ -9,41 +9,105 @@ The source code for this program is not published or otherwise divested of its t
 package transforms
 
 import (
+	"fmt"
 	"time"
 
 	v1 "k8s.io/api/core/v1"
 )
 
 // Takes a *v1.Pod and extracts the subset of properties that we care about, yielding a transforms.PodNode
-func transformPod(resource *v1.Pod) Node {
-
-	// Count the number of restarts. We define the number of Pod restarts to be the sum of the container restarts of containers in the Pod.
-	restarts := int64(0)
-	for _, containerStatus := range resource.Status.ContainerStatuses {
-		restarts += int64(containerStatus.RestartCount)
-	}
-
+func transformPod(pod *v1.Pod) Node {
+	// Loop over spec to get the container and image names
 	var containers []string
 	var images []string
-	for _, container := range resource.Spec.Containers {
+	for _, container := range pod.Spec.Containers {
 		containers = append(containers, container.Name)
 		images = append(images, container.Image)
 	}
 
-	pod := transformCommon(resource) // Start off with the common properties
-
-	// Extract the properties specific to this type
-	pod.Properties["kind"] = "Pod"
-	pod.Properties["hostIP"] = resource.Status.HostIP
-	pod.Properties["podIP"] = resource.Status.PodIP
-	pod.Properties["restarts"] = restarts
-	pod.Properties["status"] = string(resource.Status.Phase)
-	pod.Properties["container"] = containers
-	pod.Properties["image"] = images
-	pod.Properties["startedAt"] = ""
-	if resource.Status.StartTime != nil {
-		pod.Properties["startedAt"] = resource.Status.StartTime.UTC().Format(time.RFC3339)
+	// Loop over init container status or container status to get restarts and build status message
+	reason := string(pod.Status.Phase)
+	if pod.Status.Reason != "" {
+		reason = pod.Status.Reason
 	}
 
-	return pod
+	initializing := false
+	restarts := int64(0)
+	for i := range pod.Status.InitContainerStatuses {
+		container := pod.Status.InitContainerStatuses[i]
+		restarts += int64(container.RestartCount)
+		switch {
+		case container.State.Terminated != nil && container.State.Terminated.ExitCode == 0:
+			continue
+		case container.State.Terminated != nil:
+			// initialization is failed
+			if len(container.State.Terminated.Reason) == 0 {
+				if container.State.Terminated.Signal != 0 {
+					reason = fmt.Sprintf("Init:Signal:%d", container.State.Terminated.Signal)
+				} else {
+					reason = fmt.Sprintf("Init:ExitCode:%d", container.State.Terminated.ExitCode)
+				}
+			} else {
+				reason = "Init:" + container.State.Terminated.Reason
+			}
+			initializing = true
+		case container.State.Waiting != nil && len(container.State.Waiting.Reason) > 0 && container.State.Waiting.Reason != "PodInitializing":
+			reason = "Init:" + container.State.Waiting.Reason
+			initializing = true
+		default:
+			reason = fmt.Sprintf("Init:%d/%d", i, len(pod.Spec.InitContainers))
+			initializing = true
+		}
+		break
+	}
+	if !initializing {
+		restarts = int64(0)
+		hasRunning := false
+		for i := len(pod.Status.ContainerStatuses) - 1; i >= 0; i-- {
+			container := pod.Status.ContainerStatuses[i]
+
+			restarts += int64(container.RestartCount)
+			if container.State.Waiting != nil && container.State.Waiting.Reason != "" {
+				reason = container.State.Waiting.Reason
+			} else if container.State.Terminated != nil && container.State.Terminated.Reason != "" {
+				reason = container.State.Terminated.Reason
+			} else if container.State.Terminated != nil && container.State.Terminated.Reason == "" {
+				if container.State.Terminated.Signal != 0 {
+					reason = fmt.Sprintf("Signal:%d", container.State.Terminated.Signal)
+				} else {
+					reason = fmt.Sprintf("ExitCode:%d", container.State.Terminated.ExitCode)
+				}
+			} else if container.Ready && container.State.Running != nil {
+				hasRunning = true
+			}
+		}
+
+		// change pod status back to "Running" if there is at least one container still reporting as "Running" status
+		if reason == "Completed" && hasRunning {
+			reason = "Running"
+		}
+	}
+
+	if pod.DeletionTimestamp != nil && pod.Status.Reason == "NodeLost" {
+		reason = "Unknown"
+	} else if pod.DeletionTimestamp != nil {
+		reason = "Terminating"
+	}
+
+	node := transformCommon(pod) // Start off with the common properties
+
+	// Extract the properties specific to this type
+	node.Properties["kind"] = "Pod"
+	node.Properties["hostIP"] = pod.Status.HostIP
+	node.Properties["podIP"] = pod.Status.PodIP
+	node.Properties["restarts"] = restarts
+	node.Properties["status"] = reason
+	node.Properties["container"] = containers
+	node.Properties["image"] = images
+	node.Properties["startedAt"] = ""
+	if pod.Status.StartTime != nil {
+		node.Properties["startedAt"] = pod.Status.StartTime.UTC().Format(time.RFC3339)
+	}
+
+	return node
 }
