fc5fbad12f12772cff34f6b4364338e73d23bdd2
JORGE L. PADILLA <jpadilla@us.ibm.com>
2019-05-14 15:58:20 -0400

    Reduce default Helm poll time to 30 seconds (#105)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -31,7 +31,7 @@ const (
 	DEFAULT_TILLER_URL         = "tiller-deploy.kube-system:44134"
 	DEFAULT_REPORT_RATE_MS     = 5000    // 5 seconds
 	DEFAULT_HEARTBEAT_MS       = 60000   // 1 min
-	DEFAULT_HELM_PULL_MS       = 60000   // 1 min
+	DEFAULT_HELM_PULL_MS       = 30000   // 30 seconds
 	DEFAULT_MAX_BACKOFF_MS     = 3600000 // 1 hour
 	DEFAULT_REDISCOVER_RATE_MS = 60000   // 1 min
 )
