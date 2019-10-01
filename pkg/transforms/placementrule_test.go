0693818fa5872c4082fce9ac297d2a908cc8d46d
Sherin Varughese <Sherin.V@ibm.com>
2019-10-01 09:21:20 -0400

    Add placement rule test (#186)
    * Add placementrule tests
    
    * Change placement rule name

---
+++ pkg/transforms/placementrule_test.go
@@ -0,0 +1,25 @@
+/*
+IBM Confidential
+OCO Source Materials
+5737-E67
+(C) Copyright IBM Corporation 2019 All Rights Reserved
+The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
+*/
+
+package transforms
+
+import (
+	"testing"
+
+	app "github.ibm.com/IBMMulticloudPlatform/placementrule/pkg/apis/app/v1alpha1"
+)
+
+func TestTransformPlacementRule(t *testing.T) {
+	var p app.PlacementRule
+	UnmarshalFile("../../test-data/placementrule.json", &p, t)
+	node := PlacementRuleResource{&p}.BuildNode()
+
+	// Test only the fields that exist in placementrule - the common test will test the other bits
+	AssertEqual("kind", node.Properties["kind"], "PlacementRule", t)
+	AssertEqual("apigroup", node.Properties["apigroup"], "app.ibm.com", t)
+}
