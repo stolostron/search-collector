78d6cbfd706a316112297f28e0d9ad6f96d93246
Marquis Hackett <Marquis Hackett>
2020-02-14 15:28:09 -0500

    adding image tag to build.sh

--- build/build.sh
+++ build/build.sh
@@ -2,5 +2,7 @@
 
 echo " > Running build.sh"
 set -e
+
+export DOCKER_IMAGE_AND_TAG=${1}
 CGO_ENABLED=0 go build -a -v -i -installsuffix cgo -ldflags '-s -w' -o output/search-collector ./
 make docker/build
