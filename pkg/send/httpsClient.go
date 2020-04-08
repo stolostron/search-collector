88892327aef61dcf0fd2144145c8a7153941a5c8
Jorge Padilla <jpadilla@us.ibm.com>
2020-04-08 16:48:19 -0400

    Remove vulnerable TLS cipher

--- pkg/send/httpsClient.go
+++ pkg/send/httpsClient.go
@@ -56,9 +56,7 @@ func getHTTPSClient() (client http.Client) {
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			PreferServerCipherSuites: true,
 			CipherSuites: []uint16{
-				// TODO: Verify acceptable FIPS ciphers.
 				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
 				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 			},
