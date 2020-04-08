10602a647c02353f0134a00b491b9539b45cff40
Jorge Padilla <jpadilla@us.ibm.com>
2020-04-08 17:17:42 -0400

    Update httpsClient.go

--- pkg/send/httpsClient.go
+++ pkg/send/httpsClient.go
@@ -56,8 +56,7 @@ func getHTTPSClient() (client http.Client) {
 			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
 			PreferServerCipherSuites: true,
 			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,s
 			},
 			RootCAs:      caCertPool,
 			Certificates: []tls.Certificate{cert},
