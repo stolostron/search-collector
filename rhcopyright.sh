baaece7a5557470832c9edd5b9c00736bc925278
mqhackett <mahacket@redhat.com>
2020-04-29 22:23:36 -0400

    that's as helfpul as the script can be for now -- diminishing returns

--- rhcopyright.sh
+++ rhcopyright.sh
@@ -3,12 +3,9 @@
 #Loop through all files. Ignore .FILENAME types
 for f in `find . -type f -iname "*.go" ! -path "./build-harness/*" ! -path "./sslcert/*" ! -path "./test-data/*" ! -path "./vendor/*"`; do
   rhcommits=$(git --no-pager log --date=local --after="2020-03-01T16:36" --pretty=format:"%ad" $f) 
-  #echo $rhcommits
-  if [ -z "$rhcommits" ];
+  if ! [ -z "$rhcommits" ]; 
     then
-      echo "no new commits to $f"
-    else
-      echo "new commits to $f"
+      echo $f 
   fi 
 
 
