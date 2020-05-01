4ca28d7f2866693fd5084e9d2bd044e0bed419bf
Jorge Padilla <jpadilla@redhat.com>
2020-04-30 20:38:49 -0400

    Update insights.go

--- pkg/transforms/insights.go
+++ pkg/transforms/insights.go
@@ -2,6 +2,7 @@
 package transforms
 
 import (
+	"strings"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -13,19 +14,31 @@ type InsightResource struct {
 func (i InsightResource) BuildNode() Node {
 	node := transformCommon(i)         // Start off with the common properties
 
-	// description,_,_ := unstructured.NestedString(i, "spec.problem.description")
-	// fmt.Println(">>>>> Building Insight.", description)
-
-	node.Properties["kind"] = "Insight"
-	node.Properties["group"] = "open-cluster-management.io"
-	node.Properties["version"] = "v1alpha1"
 	// apiGroupVersion(i.TypeMeta, &node) // add kind, apigroup and version
-	// Extract the properties specific to this type
+	node.Properties["kind"] = i.UnstructuredContent()["kind"]
+	apiVersion := strings.Split(i.UnstructuredContent()["apiVersion"].(string), "/")
+	if len(apiVersion) == 2 {
+		node.Properties["apigroup"] = apiVersion[0]
+		node.Properties["apiversion"] = apiVersion[1]
+	} else {
+		node.Properties["apiversion"] = apiVersion[0]
+	}
 
-	node.Properties["description"] = "Problem description here."
-	node.Properties["topsolution"] = "Problem solution here."
-	node.Properties["confidence"] = 80
-	node.Properties["resolution"] = "open"
+	// Extract the properties specific to this type
+	spec, _, _ := unstructured.NestedMap(i.UnstructuredContent(), "spec")
+	problem, _, _ := unstructured.NestedMap(spec, "problem")
+	description, _, _ := unstructured.NestedString(problem, "description")
+	
+	solutions, _, _ := unstructured.NestedSlice(spec, "solutions")
+	topSolution := solutions[0].(map[string]interface{})
+
+	resolution,_,_ := unstructured.NestedString(spec, "resolution")
+	
+
+	node.Properties["description"] = description
+	node.Properties["topsolution"] = topSolution["description"]
+	node.Properties["confidence"] = topSolution["confidence"]
+	node.Properties["resolution"] = resolution
 
 	return node
 }
