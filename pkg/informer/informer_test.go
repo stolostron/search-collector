151ee72b92e9c45c027a42fac3b80f86a66ba952
dislbenn <lavontae.bennett@gmail.com>
2020-09-09 15:59:31 -0400

    adding event functions

--- pkg/informer/informer_test.go
+++ pkg/informer/informer_test.go
@@ -15,12 +15,13 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic/fake"
-	te "k8s.io/client-go/testing"
 )
 
-var dynamicClient = fake.FakeDynamicClient{}
-var gvrList = []schema.GroupVersionResource{}
-var resources []*unstructured.Unstructured
+var dynamicClient = fake.FakeDynamicClient{}                  // Fake Dynamic client that the informer will be accessing.
+var gvrList = []schema.GroupVersionResource{}                 // GroupVersionResource list.
+var resources = make(map[string][]*unstructured.Unstructured) // Key: GVR resource - Value: Test data for that resource.
+var wg = sync.WaitGroup{}                                     // Wait group to monitor the gorountines being created.
+var numOfRoutines = 0                                         // This will help us keep track of how many routines are actually still running.
 
 // The API Resource List is retreived through the discovery client
 // so since we're skipping the discovery client and just using the GVR List, we can bypass using the API List
@@ -49,8 +50,6 @@ func initAPIResources(t *testing.T) {
 			continue
 		}
 
-		t.Logf("Located file for %s resource", kind)
-
 		// Set GVR resource to current data resource.
 		var gvr schema.GroupVersionResource
 		gvr.Resource = str.Join([]string{str.ToLower(kind), "s"}, "")
@@ -68,41 +67,106 @@ func initAPIResources(t *testing.T) {
 		gvrList = append(gvrList, gvr)
 
 		// We need to create resources for the dynamic client.
-		_, err := dynamicClient.Resource(gvr).Create(data, v1.CreateOptions{})
+		dynamicClient.Resource(gvr).Create(data, v1.CreateOptions{})
 
 		if err != nil {
 			t.Fatalf("Error creating resource %s ::: failed with Error: %s", data.GetKind(), err)
 		} else {
 			// TODO: Find a more efficient way to handle resource creation for the fake client.
-			dynamicClient.Invokes(te.NewCreateAction(gvr, data.GetNamespace(), data), data)
-			resources = append(resources, data)
+			// The dynamic client is not setting the resource, so we cannot list the data from that client.
+			// Therefore, for now, we'll create a map and store the resource data under the GVR resource key.
+			resources[gvr.Resource] = append(resources[gvr.Resource], data)
 		}
 	}
 }
 
-func run(inform *GenericInformer, stopper chan struct{}) {
+// FakeRun simulate the informer run process.
+func FakeRun(t *testing.T, inform *GenericInformer, stopper chan struct{}) {
+	for {
+		t.Log("(Re)starting informer with fake client: ", inform.gvr.String())
+		FakeListAndResync(t, inform, dynamicClient) // List and Resync
+		// FakeWatch(t, inform, dynamicClient, stopper) // Watcher
+		if inform.stopped {
+			break
+		}
+	}
 
+	// Since informer has stopped, we can reduce the wg count.
+	t.Log("Informer was stopped. ", inform.gvr.String())
+	wg.Done()
+	numOfRoutines--
+
+	// If there's only one routine, we can decrement the final wg. We do this because all other routines have finished.
+	if numOfRoutines == 1 {
+		wg.Done()
+		numOfRoutines--
+	}
 }
+
+// FakeListAndResync implements a fake list and resync functionality, to mock the list and resync of the generic informer.
+func FakeListAndResync(t *testing.T, inform *GenericInformer, client fake.FakeDynamicClient) {
+	// We already stored the resources data for each informer within a map. So we can just access that within this function.
+
+	for i := range resources[inform.gvr.Resource] {
+		t.Logf("KIND: %s UUID: %s, ResourceVersion: %s",
+			inform.gvr.Resource, resources[inform.gvr.Resource][i].GetUID(), resources[inform.gvr.Resource][i].GetResourceVersion())
+		inform.AddFunc(resources[inform.gvr.Resource][i])
+		inform.resourceIndex[string(resources[inform.gvr.Resource][i].GetUID())] = resources[inform.gvr.Resource][i].GetResourceVersion()
+	}
+
+	inform.stopped = true // Stopping for dev purposes.
+}
+
+// FakeWatch implements a fake watcher for the informer, using the fake dynamic client.
+// func FakeWatch(t *testing.T, inform *GenericInformer, client fake.FakeDynamicClient, stopper chan struct{}) {
+
+// }
 
 func TestNewInformerWatcher(t *testing.T) {
 	initAPIResources(t)
 	stoppers := make(map[schema.GroupVersionResource]chan struct{})
 
-	wg := sync.WaitGroup{}
+	createFakeInformerAddHandler := func(resourceName string) func(interface{}) {
+		return func(obj interface{}) {
+			// resource := obj.(*unstructured.Unstructured)
+			t.Logf("Added")
+		}
+	}
+
+	// createFakeInformerUpdateHandler := func(resourceName string) func(interface{}, interface{}) {
+	// 	return func(oldObj, newObj interface{}) {
+	// 		// resource := newObj.(*unstructured.Unstructured)
+	// 		t.Logf("Updated")
+	// 	}
+	// }
+
+	// fakeInformerDeleteHandler := func(obj interface{}) {
+	// 	// resource := obj.(*unstructured.Unstructured)
+	// 	t.Logf("Deleted")
+	// }
+
 	go func() {
 		for {
 			if gvrList != nil {
 				// Create Informers for each test resource
 				for _, gvr := range gvrList {
+					wg.Add(1)
+					numOfRoutines++
 					t.Logf("Found resource %s, creating informer", gvr.String())
 
 					// Create informer for each GroupVersionResource
 					informer, _ := InformerForResource(gvr)
-					t.Logf("Created %s informer %+v\n\n", gvr.Resource, informer)
+					t.Logf("Created %s informer", gvr.Resource)
+
+					// Set the fake informer functions
+					informer.AddFunc = createFakeInformerAddHandler(gvr.Resource)
+					// informer.UpdateFunc = createFakeInformerUpdateHandler(gvr.Resource)
+					// informer.DeleteFunc = fakeInformerDeleteHandler
 
 					// In the test, we can simulate that the informer stopped. Allowing us to test the retry logic.
 					stopper := make(chan struct{})
 					stoppers[gvr] = stopper
+					go FakeRun(t, &informer, stopper)
 				}
 				t.Log("Total test informers running: ", len(stoppers))
 			}
@@ -110,10 +174,11 @@ func TestNewInformerWatcher(t *testing.T) {
 			break
 		}
 		// After we handle every event and finish with the watcher and informer, we can exit out the test.
-		wg.Done()
 	}()
 	// Similarly to how we keep the transformer routines running, we'll keep the test informers running.
 	// However, after the test conditions we can decrement the waitgroup and exit the test.
+
 	wg.Add(1)
+	numOfRoutines++
 	wg.Wait()
 }
