fcf5a439109598a0b31d48a28e74d4214bfbc97d
Jorge Padilla <jpadilla@redhat.com>
2020-09-11 13:17:11 -0400

    Fix lint

--- pkg/informer/informer_test.go
+++ pkg/informer/informer_test.go
@@ -25,17 +25,21 @@ func fakeDynamicClient() *fake.FakeDynamicClient {
 	)
 }
 
-func generateSimpleEvent(informer GenericInformer) {
+func generateSimpleEvent(informer GenericInformer, t *testing.T) {
 	// Add resource. Generates ADDED event.
 	gvr := schema.GroupVersionResource{Group: "open-cluster-management.io", Version: "v1", Resource: "thekinds"}
 	newResource := newTestUnstructured("open-cluster-management.io/v1", "TheKind", "ns-foo", "name-new", "id-999")
-	informer.client.Resource(gvr).Namespace("ns-foo").Create(newResource, v1.CreateOptions{})
+	_, err1 := informer.client.Resource(gvr).Namespace("ns-foo").Create(newResource, v1.CreateOptions{})
 
 	// Update resource. Generates MODIFIED event.
-	informer.client.Resource(gvr).Namespace("ns-foo").Update(newResource, v1.UpdateOptions{})
+	_, err2 := informer.client.Resource(gvr).Namespace("ns-foo").Update(newResource, v1.UpdateOptions{})
 
 	// Delete resource. Generated DELETED event.
-	informer.client.Resource(gvr).Namespace("ns-foo").Delete("name-bar2", &v1.DeleteOptions{})
+	err3 := informer.client.Resource(gvr).Namespace("ns-foo").Delete("name-bar2", &v1.DeleteOptions{})
+
+	if err1 != nil || err2 != nil || err3 != nil {
+		t.Error("Error generating mocked events.")
+	}
 }
 
 func newTestUnstructured(apiVersion, kind, namespace, name, uid string) *unstructured.Unstructured {
@@ -112,7 +116,7 @@ func Test_Run(t *testing.T) {
 	go informer.Run(stopper)
 	time.Sleep(10 * time.Millisecond)
 
-	generateSimpleEvent(informer)
+	generateSimpleEvent(informer, t)
 	time.Sleep(10 * time.Millisecond)
 
 	stopper <- struct{}{}
@@ -162,7 +166,7 @@ func Test_watch(t *testing.T) {
 	go informer.watch(stopper)
 	time.Sleep(10 * time.Millisecond)
 
-	generateSimpleEvent(informer)
+	generateSimpleEvent(informer, t)
 	time.Sleep(10 * time.Millisecond)
 
 	// Simulate that the informer has been stopped successfully.
