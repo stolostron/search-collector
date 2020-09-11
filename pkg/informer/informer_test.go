125a42ce62d4799886a09bbd8227fdf22880fa19
Jorge Padilla <jpadilla@redhat.com>
2020-09-11 13:00:28 -0400

    Reduce wait times

--- pkg/informer/informer_test.go
+++ pkg/informer/informer_test.go
@@ -86,7 +86,6 @@ func Test_listAndResync(t *testing.T) {
 
 // Verify that DeleteFunc is called for indexed resources that no longer exist.
 func Test_listAndResync_syncWithPrevState(t *testing.T) {
-
 	// Create informer instance to test.
 	informer, _, deleteFuncCount, _ := initInformer()
 
@@ -103,24 +102,30 @@ func Test_listAndResync_syncWithPrevState(t *testing.T) {
 	}
 }
 
+// Verify the informer's Run function.
 func Test_Run(t *testing.T) {
 	// Create informer instance to test.
 	informer, addFuncCount, deleteFuncCount, updateFuncCount := initInformer()
 
 	// Start informer routine
-	go informer.Run(make(chan struct{}))
+	stopper := make(chan struct{})
+	go informer.Run(stopper)
 	time.Sleep(10 * time.Millisecond)
 
 	generateSimpleEvent(informer)
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(10 * time.Millisecond)
 
-	// Verify that informer.AddFunc is called for each of the mocked resources (5 times).
+	stopper <- struct{}{}
+
+	// Verify that informer.AddFunc is called for each of the mocked resources (6 times).
 	if *addFuncCount != 6 {
 		t.Errorf("Expected informer.AddFunc to be called 6 times, but got %d.", *addFuncCount)
 	}
+	// Verify informer.UpdateFunc is called once.
 	if *updateFuncCount != 1 {
 		t.Errorf("Expected informer.UpdateFunc to be called 1 times, but got %d.", *updateFuncCount)
 	}
+	// Verify informer.DeleteFunc is called once.
 	if *deleteFuncCount != 1 {
 		t.Errorf("Expected informer.DeleteFunc to be called 1 times, but got %d.", *deleteFuncCount)
 	}
@@ -131,18 +136,18 @@ func Test_Run_retryBackoff(t *testing.T) {
 	// Create informer instance to test.
 	informer, _, _, _ := initInformer()
 
-	informer.retries = 2
+	informer.retries = 1
 	startTime := time.Now()
 	retryTime := time.Now() // Initializing to now ensures that the test fail if AddFunc is not called in the expected time.
 	informer.AddFunc = func(interface{}) { retryTime = time.Now() }
 
 	// Execute function
 	go informer.Run(make(chan struct{}))
-	time.Sleep(5 * time.Second)
+	time.Sleep(2010 * time.Millisecond)
 
-	// Verify backoff logic waits 4 seconds before retrying.
-	if startTime.Add(4 * time.Second).After(retryTime) {
-		t.Errorf("Backoff logic failed to wait for 4 seconds.")
+	// Verify backoff logic waits 2 seconds before retrying.
+	if startTime.Add(2 * time.Second).After(retryTime) {
+		t.Errorf("Backoff logic failed to wait for 2 seconds.")
 	}
 }
 
@@ -158,11 +163,11 @@ func Test_watch(t *testing.T) {
 	time.Sleep(10 * time.Millisecond)
 
 	generateSimpleEvent(informer)
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(10 * time.Millisecond)
 
 	// Simulate that the informer has been stopped successfully.
 	stopper <- struct{}{}
-	time.Sleep(5 * time.Second)
+	time.Sleep(10 * time.Millisecond)
 
 	if !informer.stopped {
 		t.Errorf("Expected informer.stopped to be true, but got %t", informer.stopped)
