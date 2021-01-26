6819b19135dede3a166853dbb2084614c94ebebb
Jorge Padilla <jpadilla@redhat.com>
2021-01-26 09:57:13 -0500

    Increase default Heartbeat and rediscover rate

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,7 +4,7 @@ OCO Source Materials
 (C) Copyright IBM Corporation 2019 All Rights Reserved
 The source code for this program is not published or otherwise divested of its trade secrets,
 irrespective of what has been deposited with the U.S. Copyright Office.
-Copyright (c) 2020 Red Hat, Inc.
+Copyright (c) 2020, 2021 Red Hat, Inc.
 */
 
 package config
@@ -32,10 +32,10 @@ const (
 	DEFAULT_AGGREGATOR_HOST    = "https://localhost"
 	DEFAULT_AGGREGATOR_PORT    = "3010"
 	DEFAULT_CLUSTER_NAME       = "local-cluster"
-	DEFAULT_HEARTBEAT_MS       = 60000  // 1 min
-	DEFAULT_MAX_BACKOFF_MS     = 600000 // 10 min
-	DEFAULT_REDISCOVER_RATE_MS = 60000  // 1 min
-	DEFAULT_REPORT_RATE_MS     = 5000   // 5 seconds
+	DEFAULT_HEARTBEAT_MS       = 300000  // 5 min
+	DEFAULT_MAX_BACKOFF_MS     = 600000  // 10 min
+	DEFAULT_REDISCOVER_RATE_MS = 120000  // 2 min
+	DEFAULT_REPORT_RATE_MS     = 5000    // 5 seconds
 	DEFAULT_RUNTIME_MODE       = "production"
 	DEFAULT_TILLER_HOST        = "tiller-deploy.kube-system" // Deprecated in 2.1
 	DEFAULT_TILLER_PORT        = "44134"                     // Deprecated in 2.1
