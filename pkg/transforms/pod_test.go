944df4f2d966037e5f4bc3d161e4c070b0ad8280
Jorge Padilla <jpadilla@redhat.com>
2021-02-18 17:43:57 -0500

    Pod in init state

--- pkg/transforms/pod_test.go
+++ pkg/transforms/pod_test.go
@@ -35,3 +35,23 @@ func TestTransformPod(t *testing.T) {
 	AssertEqual("startedAt", node.Properties["startedAt"], date.UTC().Format(time.RFC3339), t)
 	AssertEqual("status", node.Properties["status"], string(v1.PodRunning), t)
 }
+
+func TestTransformPodInitState(t *testing.T) {
+	var p v1.Pod
+	UnmarshalFile("pod-initializing.json", &p, t)
+	node := PodResourceBuilder(&p).BuildNode()
+
+	// Build time struct matching time in test data
+	date := time.Date(2019, 02, 21, 21, 30, 33, 0, time.UTC)
+
+	// Test only the fields that exist in pods - the common test will test the other bits
+
+	AssertEqual("kind", node.Properties["kind"], "Pod", t)
+	AssertEqual("hostIP", node.Properties["hostIP"], "1.1.1.1", t)
+	AssertEqual("podIP", node.Properties["podIP"], "2.2.2.2", t)
+	AssertEqual("restarts", node.Properties["restarts"], int64(2), t)
+	AssertDeepEqual("container", node.Properties["container"], []string{"fake-pod"}, t)
+	AssertDeepEqual("image", node.Properties["image"], []string{"fake-image:0.5.0.1"}, t)
+	AssertEqual("startedAt", node.Properties["startedAt"], date.UTC().Format(time.RFC3339), t)
+	AssertEqual("status", node.Properties["status"], "Init:CrashLoopBackOff", t)
+}
