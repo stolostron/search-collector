8c02a0f3475b4338fa4643d777d060fe04032d13
Sherin Varughese <shvarugh@redhat.com>
2022-07-20 17:16:09 -0400

    update lint script
    Signed-off-by: Sherin Varughese <shvarugh@redhat.com>

--- build/run-lint-check.sh
+++ build/run-lint-check.sh
@@ -5,4 +5,4 @@ GOPATH=$(go env GOPATH)
 export GOFLAGS=""
 DOWNLOAD_URL="https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh"
 curl -sSfL "${DOWNLOAD_URL}" | sh -s -- -b "${GOPATH}/bin" v1.47.1
-$(GOLANGCI_LINT_CACHE=${GOLANGCI_LINT_CACHE} CGO_ENABLED=0 GOGC=25 golangci-lint run)
+GOLANGCI_LINT_CACHE=${GOLANGCI_LINT_CACHE} CGO_ENABLED=0 GOGC=25 golangci-lint run
