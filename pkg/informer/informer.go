b4e3137fe2ad8ab21942e569251c44a62a19ff3e
Anxhela <acoba@redhat.com>
2022-10-26 16:05:24 -0400

    move comment
    Signed-off-by: Anxhela <acoba@redhat.com>

--- pkg/informer/informer.go
+++ pkg/informer/informer.go
@@ -55,9 +55,9 @@ func (inform *GenericInformer) Run(stopper chan struct{}) {
 				glog.V(5).Infof("Stopping informer %s and removing resource with UID: %s", inform.gvr.Resource, key)
 				obj := newUnstructured(inform.gvr.Resource, key)
 				inform.DeleteFunc(obj)
-				glog.V(5).Info("Informer stopped. ", inform.gvr.String())
 
 			}
+			glog.V(5).Info("Informer stopped. ", inform.gvr.String())
 
 			return
 		default:
