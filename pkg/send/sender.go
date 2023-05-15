1b014491b3d1bcdd3655d6fe30bf20edb480b4ef
Jorge Padilla <jpadilla@redhat.com>
2023-05-15 13:38:23 -0400

    ACM-5188 Fix sonar security finding
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/send/sender.go
+++ pkg/send/sender.go
@@ -200,7 +200,7 @@ func (s *Sender) send(payload Payload, expectedTotalResources int, expectedTotal
 	}
 	payloadBuffer := bytes.NewBuffer(payloadBytes)
 	resp, err := s.httpClient.Post(s.aggregatorURL+s.aggregatorSyncPath, "application/json", payloadBuffer)
-	if resp != nil {
+	if resp != nil && resp.Body != nil {
 		defer resp.Body.Close()
 	}
 	if err != nil {
