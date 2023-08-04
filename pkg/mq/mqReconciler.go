b36050429bb1998895000985da1a15d509bd6ab6
Jorge Padilla <jpadilla@redhat.com>
2023-08-04 16:56:41 -0400

    Saving progress
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

--- pkg/mq/mqReconciler.go
+++ pkg/mq/mqReconciler.go
@@ -1,53 +1,50 @@
 package mq
 
 import (
-	"crypto/sha256"
+	"encoding/json"
 	"fmt"
+	"hash/fnv"
 
 	tr "github.com/stolostron/search-collector/pkg/transforms"
 	"k8s.io/klog/v2"
 )
 
-type MqMessage struct {
-	uid        string
-	kindPlural string
-	properties map[string]interface{}
-	sha        string
+// Simple non-cryptographic hash function.
+func hash(props map[string]interface{}) uint64 {
+	algorithm := fnv.New64a()
+	algorithm.Write([]byte(fmt.Sprintf("%s", props)))
+	return algorithm.Sum64()
 }
-
-type Store struct {
-	resources map[string]*MqMessage
-}
-
-var store Store
 
 func MQReconciler(resources chan (tr.NodeEvent)) {
 	for {
 		res := <-resources
 
-		newShaBytes := sha256.Sum256([]byte(fmt.Sprintf("%s", res.Node.Properties)))
-		newSha := fmt.Sprintf("%x", newShaBytes)
-		klog.Infof("Procesing NodeEvent with sha: %x\n", newSha)
+		newHash := hash(res.Node.Properties)
 
 		// Check if resource is in store
 		if r, ok := store.resources[res.Node.UID]; ok {
-			// If it is, compare sha to check if it's the same.
-			if r.sha == newSha {
-				// If it is, skip
-				klog.Infof("Skipping NodeEvent with sha: %x\n", newSha)
+			// Skip if resource is in the store and has the same Hash.
+			if r.Hash == newHash {
+				klog.Infof("Skipping NodeEvent because Hash matches. %x\n", newHash)
 				continue
 			} else {
-				// If it's not, update store and send to mq
+				// Resource is in the store but needs to be updated because Hash didn't match.
 				newMqResource := MqMessage{
-					uid:        res.Node.UID,
-					kindPlural: res.Node.ResourceString,
-					properties: res.Node.Properties,
-					sha:        newSha,
+					UID:        res.Node.UID,
+					Properties: res.Node.Properties,
+					Hash:       newHash,
 				}
 
-				klog.Infof("Sending UPDATE to mq:\n%+v\n", newMqResource)
+				klog.Infof("Sending UPDATE message to mq. Kind: %s\t Name: %s\n", newMqResource.Properties["kind"], newMqResource.Properties["name"])
 				// TODO: send only the changed bits to mq.
-				if err := SendMessage(res.Node.UID, fmt.Sprintf("%s", newMqResource)); err == nil { // TODO: send a json string
+
+				jsonBytes, jsonErr := json.Marshal(newMqResource)
+				if jsonErr != nil {
+					klog.Errorf("Error marshalling resource to json: %s", jsonErr)
+					continue
+				}
+				if err := SendMessage(res.Node.UID, string(jsonBytes)); err == nil {
 
 					// Update store
 					store.resources[res.Node.UID] = &newMqResource
@@ -56,24 +53,27 @@ func MQReconciler(resources chan (tr.NodeEvent)) {
 				}
 			}
 		} else {
+			// Resource doesn't exist in store. Send message to mq and add to the store.
 			newMqResource := MqMessage{
-				uid:        res.Node.UID,
-				kindPlural: res.Node.ResourceString,
-				properties: res.Node.Properties,
-				sha:        newSha,
+				UID:        res.Node.UID,
+				Properties: res.Node.Properties,
+				Hash:       newHash,
 			}
-			klog.Infof("Sending CREATE to mq:\n%+v\n", newMqResource)
+			klog.Infof("Sending CREATE to mq. Kind: %s\t Name: %s\n", newMqResource.Properties["kind"], newMqResource.Properties["name"])
 
-			// send resource to mq.
-			if err := SendMessage(res.Node.UID, fmt.Sprintf("%s", newMqResource)); err == nil { // TODO: send a json string
+			jsonBytes, jsonErr := json.Marshal(newMqResource)
+			if jsonErr != nil {
+				klog.Errorf("Error marshalling resource to json: %s", jsonErr)
+				continue
+			}
 
-				// Add to store
+			if err := SendMessage(res.Node.UID, string(jsonBytes)); err == nil {
 				store.resources[res.Node.UID] = &newMqResource
 			} else {
 				klog.Errorf("Error sending NEW message to mq: %s", err)
 			}
 		}
 
-		klog.Infof("Done processing NodeEvent.")
+		// klog.Infof("Done processing NodeEvent.")
 	}
 }
