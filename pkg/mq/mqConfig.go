ff94bc9eefdb2f58db6b61d442b8607f550997d3
Jorge Padilla <jpadilla@redhat.com>
2023-08-07 16:17:17 -0400

    Simplify config
    Signed-off-by: Jorge Padilla <jpadilla@redhat.com>

---
+++ pkg/mq/mqConfig.go
@@ -0,0 +1,9 @@
+// Copyright Contributors to the Open Cluster Management project
+package mq
+
+var (
+	brokerList = []string{""}
+	topic      = "cluster.A"
+	maxRetry   = 3
+	partition  = int32(0)
+)
