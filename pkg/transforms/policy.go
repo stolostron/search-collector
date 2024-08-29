c9bd4b7a6ad4167cbfa7c747078b3c7e85d66945
Yi Rae Kim <yikim@redhat.com>
2024-08-28 22:49:39 -0400

    Add config/operator/cert policy transform (#298)
    Add properties such as responseAction, compliant, annotations, severity, parentPolicyName, and parentPolicyNs to config/operator/cert policy.
    Ref: https://issues.redhat.com/browse/ACM-13031
    Signed-off-by: yiraeChristineKim <yikim@redhat.com>

--- pkg/transforms/policy.go
+++ pkg/transforms/policy.go
@@ -11,7 +11,11 @@ Copyright (c) 2020 Red Hat, Inc.
 package transforms
 
 import (
-	p "github.com/stolostron/governance-policy-propagator/api/v1"
+	"strings"
+
+	policy "github.com/stolostron/governance-policy-propagator/api/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 // PolicyResource ...
@@ -20,7 +24,7 @@ type PolicyResource struct {
 }
 
 // PolicyResourceBuilder ...
-func PolicyResourceBuilder(p *p.Policy) *PolicyResource {
+func PolicyResourceBuilder(p *policy.Policy) *PolicyResource {
 	node := transformCommon(p)         // Start off with the common properties
 	apiGroupVersion(p.TypeMeta, &node) // add kind, apigroup and version
 	// Extract the properties specific to this type
@@ -37,6 +41,93 @@ func PolicyResourceBuilder(p *p.Policy) *PolicyResource {
 	if okns && okpp {
 		node.Properties["_parentPolicy"] = pnamespace + "/" + ppolicy
 	}
+
+	return &PolicyResource{node: node}
+}
+
+// For cert, config, operator policies.
+// This function returns `annotations`, `_isExternal` for `source`,
+// and `severity`, `compliant`, and `remediationAction`.
+func getPolicyCommonProperties(c *unstructured.Unstructured, node Node) Node {
+	node.Properties["_isExternal"] = false
+
+	for _, m := range c.GetManagedFields() {
+		if m.Manager == "multicluster-operators-subscription" ||
+			strings.Contains(m.Manager, "argocd") {
+			node.Properties["_isExternal"] = true
+
+			break
+		}
+	}
+
+	typeMeta := metav1.TypeMeta{
+		Kind:       c.GetKind(),
+		APIVersion: c.GetAPIVersion(),
+	}
+
+	apiGroupVersion(typeMeta, &node) // add kind, apigroup and version
+
+	node.Properties["compliant"], _, _ = unstructured.NestedString(c.Object, "status", "compliant")
+
+	node.Properties["severity"], _, _ = unstructured.NestedString(c.Object, "spec", "severity")
+
+	node.Properties["remediationAction"], _, _ = unstructured.NestedString(c.Object, "spec", "remediationAction")
+
+	node.Properties["disabled"], _, _ = unstructured.NestedBool(c.Object, "spec", "disabled")
+
+	return node
+}
+
+// For cert, config policies.
+func StandalonePolicyResourceBuilder(c *unstructured.Unstructured) *PolicyResource {
+	node := transformCommon(c) // Start off with the common properties
+
+	return &PolicyResource{node: getPolicyCommonProperties(c, node)}
+}
+
+func OperatorPolicyResourceBuilder(c *unstructured.Unstructured) *PolicyResource {
+	node := transformCommon(c) // Start off with the common properties
+
+	node = getPolicyCommonProperties(c, node)
+
+	var deploymentAvailable bool
+	var upgradeAvailable bool
+
+	conditions, _, _ := unstructured.NestedSlice(c.Object, "status", "conditions")
+	for _, condition := range conditions {
+		mapCondition, ok := condition.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		conditionType, found, err := unstructured.NestedString(mapCondition, "type")
+		if !found || err != nil {
+			continue
+		}
+
+		conditionReason, found, err := unstructured.NestedString(mapCondition, "reason")
+		if !found || err != nil {
+			continue
+		}
+
+		if conditionType == "InstallPlanCompliant" {
+			if conditionReason == "InstallPlanRequiresApproval" || conditionReason == "InstallPlanApproved" {
+				upgradeAvailable = true
+			} else {
+				upgradeAvailable = false
+			}
+		} else if conditionType == "DeploymentCompliant" {
+			if conditionReason == "DeploymentsAvailable" {
+				deploymentAvailable = true
+			} else {
+				deploymentAvailable = false
+			}
+		}
+	}
+
+	node.Properties["deploymentAvailable"] = deploymentAvailable
+	node.Properties["upgradeAvailable"] = upgradeAvailable
+
 	return &PolicyResource{node: node}
 }
 
@@ -47,6 +138,6 @@ func (p PolicyResource) BuildNode() Node {
 
 // BuildEdges construct the edges for Policy Resources
 func (p PolicyResource) BuildEdges(ns NodeStore) []Edge {
-	//no op for now to implement interface
+	// no op for now to implement interface
 	return []Edge{}
 }
